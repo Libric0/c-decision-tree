88f52726bf908b5f271d4e65165b8eca656be8b2
Fredi <fredrik.konrad@rwth-aachen.de>
2023-04-02 22:39:40 +0200

    Added visualization and readme

--- README.md
+++ README.md
@@ -1,2 +1,114 @@
 # c-decision-tree
-A simple categorial decision tree library for python users. The syntax is very similar to scikit learn.
+A simple categorial decision tree library for python users, mainly focused at educators and students. The syntax is very similar to scikit learn and it has built-in visualization.
+
+![Decision Tree Visualized](/assets/Diet.svg)
+
+## Features
+- Creates an entropy-based decision tree
+- Pre-pruning using `min_instances`, `max_depth`, `min_gain`
+- Supports lists of instances as well as `pandas.DataFrame`
+- Built-in Visualization
+  - IPython Notebook Support
+  - Seaborn color palettes support
+
+## Dependencies
+
+### Soft Dependencies
+- `seaborn >= 0.11.2`, only needed to access seaborn's color palettes
+
+# Usage Example
+
+In this example, we are going to generate a decisiontree that classifies all dishes in the following dataframe `df` into the categories Vegan, Vegetarian, and Meat-Based.
+<details><summary>Click to show the Dataframe</summary>
+
+|    | Name                     | Dairy   | Meat   | Eggs   | Vegetables   | Fruits   | Legumes   | Diet       |
+|---:|:-------------------------|:--------|:-------|:-------|:-------------|:---------|:----------|:-----------|
+|  0 | Margherita Pizza         | Yes     | No     | No     | Yes          | No       | No        | Vegetarian |
+|  1 | Grilled Chicken Salad    | No      | Yes    | No     | Yes          | No       | No        | Meat-Based |
+|  2 | Lentil Soup              | No      | No     | No     | Yes          | No       | Yes       | Vegan      |
+|  3 | Cheese Omelette          | Yes     | No     | Yes    | No           | No       | No        | Vegetarian |
+|  4 | Veggieburger             | No      | No     | No     | Yes          | No       | Yes       | Vegan      |
+|  5 | Beef and Bean Burrito    | Yes     | Yes    | No     | Yes          | No       | Yes       | Meat-Based |
+|  6 | Falafel                  | No      | No     | No     | Yes          | No       | Yes       | Vegan      |
+|  7 | Fruit Salad              | No      | No     | No     | No           | Yes      | No        | Vegan      |
+|  8 | Greek Salad              | Yes     | No     | No     | Yes          | No       | No        | Vegetarian |
+|  9 | Vegetable Curry          | No      | No     | No     | Yes          | Yes      | No        | Vegan      |
+| 10 | Egg Fried Rice           | No      | No     | Yes    | Yes          | No       | No        | Vegetarian |
+| 11 | Scrambled Tofu           | No      | No     | No     | No           | No       | Yes       | Vegan      |
+| 12 | Tofu Stir Fry            | No      | No     | No     | Yes          | No       | Yes       | Vegan      |
+</details>
+
+## Setup
+First, we import and initialize the decision tree
+
+```python
+from c_DecisionTree import DecisionTree
+dt = DecisionTree()
+```
+
+Next, fit the decision tree by calling the `fit` function with the DataFrame and the target column `"Diet"` as arguments. Make sure that unique identifiers are excluded from the column.
+
+```python
+dt.fit(data=df.loc[:, df.columns != "Name"], target="Diet")
+```
+This will create the following decision tree:
+
+![First Example Tree](assets/diet_example.svg)
+
+Alternatively, you could pass a list of instances to `X` and a list of labels to `y`, just as seen as in scikit learn. The instances in `X` would then just be a list of feature values. In this case, you have to make sure that the order of your features don't change. 
+
+## Visualization
+
+All examples show a SVG of the decision tree. Each node displays the **number of instances** in the middle. Around this one can see the **relative frequencies** of each label in the node. The outer border is displayed in the color of the **majority vote**. 
+
+To display the tree `dt` in a jupyter notebook, simply write this as the last line of a cell:
+
+```python
+dt
+```
+
+Jupyter always displays whatever the last line of a cell returns, or in this case the decision tree's SVG representation. You should now see something like this:
+
+![Decision Tree using the default Palette](assets/diet_example_default_palette.svg)
+
+This tree looks a bit different than the examples so far. The color for Vegan and Vegetarian are swapped. As a vegan diet is often seen as more environmentally friendly, it can make sense to swap them back. This can easily be done by **changing the palette**. To this end, simply call the `set_palette` function:
+
+```python
+from c_DecisionTree import set_palette
+set_palette(['hsl(25, 67%, 61%)', 'hsl(83, 67%, 61%)', 'hsl(234, 67%, 61%)'])
+```
+
+If you now redraw the tree, it should have the colors swapped again.
+
+The palette can be changed in 3 ways
+- Set back to Default: `set_palette(None)`
+- Use a custom palette: `set_palette(list)`
+  - The `list` should contain colors as strings that are understandable to an SVG parser. For example `"#55443F"`, `"rgb(255,20,70)"`, or `"hsl(25, 67%, 61%)"`
+- Use a seaborn palette: `set_palette(name)`
+  - For example `set_palette("muted")`
+## Inference
+Now, we want to classify a dish that is not in our dataset. 
+|    | Name                     | Dairy   | Meat   | Eggs   | Vegetables   | Fruits   | Legumes   |
+|---:|:-------------------------|:--------|:-------|:-------|:-------------|:---------|:----------|
+|  | Shrimp and Avocado Salad | No      | Yes    | No     | Yes          | No       | No        |
+
+This can simply be done by passing the dataframe containing this instance to the `predict` function of the tree. Assuming that our new instance are in the dataset `instances`, this would look like that:
+
+```python
+dt.predict(data=instances)
+```
+
+This then returns the list `['Meat-Based']`. It classified our dish correctly!
+
+Again, you could alternatively just pass a list of instances to `X`, as one would do in scikit learn, but you would have to make sure that the sorting is the same as the list that was used to create the tree. You can check the order using `dt.feature_names`.
+## Pre-Pruning
+This decision tree is technically very correct, but the split on eggs only has one instance in the "Yes" option. By changing the parameters of the decision tree, one can make use of pre-pruning. Depending on the task, this can improve generalization performance. There are currently 3 parameters:
+- `min_instances`: A split can only be made if all children contain at least this many instances.
+- `min_gain`: A split can only be made if the split would reduce the entropy of this node by this amount.
+- `max_depth`: Only allows splits up to this depth. For `max_depth=2`, the tree in the above example would have one split less.
+
+As an example, we decide to only allow a split if all children have at least 2 instances.
+
+![Pruned Tree with min_instances = 2](assets/diet_example_prune.svg)
+
+In our example, this performs worse. In practice however, this can lead to decision trees that generalize better.
--- c_DecisionTree.py
+++ c_DecisionTree.py
@@ -6,12 +6,92 @@
 # The c-decision-tree library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 
 # You should have received a copy of the GNU General Public License along with FocusLocus. If not, see <https://www.gnu.org/licenses/>.
-from math import log2
-import pandas as pd
+
+from math import log2, sin, cos, pi
+
+_palette = None
+
+def set_palette(palette):
+    global _palette
+    _palette = palette
+
+def _standard_palette(n_colors):
+    return [f"hsl({((((i)*209)%360) + 25) % 360}, 67%, 61%)" for i in range(n_colors)]
+
+def _color_palette(n_colors):
+    # Standard palette given by the library, 360 different hues but gets crammed / hard to distinguish after 10
+    if _palette is None:
+        return _standard_palette(n_colors)
+    
+    # Use user-defined palettes
+    if isinstance(_palette, list):
+        return (_palette*(round(n_colors/len(_palette)+1)))[:n_colors]
+    
+    # Use seaborn-palette for string
+    if isinstance(_palette, str):
+        try:
+            import seaborn as sns
+        except ImportError:
+            raise Exception("Named palettes can only be used if seaborn is installed in your python environment. Run `pip install seaborn` to install it.")
+        # Converting to SVG rgb format
+        return [f'rgb({color[0]*255},{color[1]*255},{color[2]*255})' for color in sns.color_palette(palette = _palette, n_colors = n_colors)]
+    import warnings
+    warnings.warn("The user-defined palette is not valid. Resorting to default.")
+    return _standard_palette(n_colors)
+    
+
+def _shorten_number(num):
+        if num < 1000:
+            return num
+        if num < 1_000_000:
+            return f"{round(num/1_000)}K"
+        if num < 1_000_000_000:
+            return f"{round(num/1_000_000)}M"
+        if num < 1_000_000_000_000:
+            return f"{round(num/1_000_000_000)}B"
+        if num < 1_000_000_000_000_000:
+            return f"{round(num/1_000_000_000_000)}T"
+        return f"{round(num/1_000_000_000_000_000)}Q"
+
+def _pie_chart(cx, cy, r, percentages:dict):
+    '''Returns SVG code to draw a pie chart using a rainbow pallette'''
+    ret = ''
+    # Sorting the items to get a deterministic view of the dict
+    percentages_sorted = sorted(percentages.items())
+    # Retrieving the maximum to mark the chart with the majority vote
+    max_item = max(enumerate(percentages_sorted), key=lambda item: item[1][1])
+
+    # Drawing the outer circle indicating the majority vote
+    ret += f'<circle cx="{cx}" cy="{cy}" r="{r}" fill="{_color_palette(len(percentages))[max_item[0]]}"/>'
+    # Drawing the seperating white circle
+    ret += f'<circle cx="{cx}" cy="{cy}" r="{r*0.9}" fill="hsl({0},0%,100%)"/>'
+
+    # If we have no entropy, we just draw a complete circle
+    if max_item[1][1] == 1:
+        ret += f'<circle cx="{cx}" cy="{cy}" r="{r*0.85}" fill="{_color_palette(len(percentages))[max_item[0]]}"/>'
+        # Draw white circle in the middle to allow for text
+        ret += f'<circle cx="{cx}" cy="{cy}" r="{r*0.6}" fill="hsl({0},0%,100%)"/>'
+        return ret
+    
+    # Draw Actual pie chard based on percentages
+    total = -.25
+    for i, percentage in enumerate(percentages_sorted):
+        # Very long string. Long explanation. We draw a path from the center, over an arc, back to the center.
+        # M moves the cursor to the center
+        # L draws a line to the start of the label's segment
+        # A draws an art from the start to the end of the segment
+        # - The "if" is needed, because in the case of >0.5 percentage, we have to choose the longer arc
+        # z moves the cursor back to the center and closes the arc
+        ret += f'<path d="M{cx},{cy} L{round(cos(total*2*pi)*r*0.85+cx,10)},{round(sin(total*2*pi)*r*0.85+cy,10)} A{r*0.85},{r*0.85} 0 {0 if percentage[1] <= 0.5 else 1},1 {round(cos((total+percentage[1])*2*pi)*r*0.85+cx,10)},{round(sin((total+percentage[1])*2*pi)*r*0.85+cy,10)} z" fill="{_color_palette(len(percentages))[i]}" stroke="{_color_palette(len(percentages))[i]}" stroke-width="0.1"/>'
+        total += percentage[1]
+    
+    # Draw white circle in the middle to allow for text
+    ret += f'<circle cx="{cx}" cy="{cy}" r="{r*0.6}" fill="hsl({0},0%,100%)"/>'
+    return ret
 
 class _DecisionTreeNode:
 
-    def __init__(self, X:list, y:list, feature_names:list, feature_splitable:list):
+    def __init__(self, X:list, y:list, feature_names:list, feature_splitable:list, value=None):
         '''A single node of a decision Tree.
         
         - `X` is the list of instances without the target feature
@@ -30,6 +110,7 @@ class _DecisionTreeNode:
         self.labels = set(y)
         self.feature_names = feature_names
         self.feature_splitable = feature_splitable
+        self.value = value
 
         self.split_feature = None
         self.children = None
@@ -47,6 +128,15 @@ class _DecisionTreeNode:
     def split_feature_index(self):
         return self.feature_names.index(self.split_feature)
     
+    @property
+    def label_percentages(self):
+        label_counts = {label:0.0 for label in self.labels}
+        for target in self.y:
+            label_counts[target] += 1
+        for label in label_counts.keys():
+            label_counts[label] /= len(self.y)
+        return label_counts
+
     def getNodeMajorityLabel(self):
         '''Returns the majority label based on the training data'''
         max_count = 0
@@ -106,7 +196,7 @@ class _DecisionTreeNode:
         new_splitable[feature_index] = False
 
         # Creating child node for each feature value
-        children = {feature_value: _DecisionTreeNode(X=children_X[feature_value], y=children_Y[feature_value], feature_names=self.feature_names, feature_splitable = new_splitable) for feature_value in feature_values}
+        children = {feature_value: _DecisionTreeNode(X=children_X[feature_value], y=children_Y[feature_value], feature_names=self.feature_names, feature_splitable = new_splitable, value=feature_value) for feature_value in feature_values}
         return children
     
     def getInformationGain(self, split):
@@ -151,6 +241,48 @@ class _DecisionTreeNode:
             for child in self.children.values():
                 child.id3(min_gain, min_instances, max_depth = max_depth, depth=depth+1)
         return self
+    
+    
+    def node_svg(self, cx, cy, root_labels):
+        x_translate = f'x="{cx}"' if cx != 0 else ""
+        y_translate = f'y="{cy}"' if cy != 0 else ""
+        ret = f'<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 80 120" width="80" height="120" {x_translate} {y_translate}>'
+        label_percentages = {label:0 for label in root_labels}
+        label_percentages.update(self.label_percentages)
+        if self.value != None:
+            ret += f'<text x="{40}" y="{10}" font-family="Arial, Helvetica, sans-serif" dominant-baseline="central" text-anchor="middle">{self.value}</text>'
+
+        ret += _pie_chart(40, 50, 30, label_percentages)
+        ret += f'<text x="{40}" y="{50}" font-family="Arial, Helvetica, sans-serif" dominant-baseline="central" text-anchor="middle">{_shorten_number(self.data_set_size)}</text>'
+        if self.split_feature != None:
+            ret += f'<text x="{40}" y="{90}" font-family="Arial, Helvetica, sans-serif" dominant-baseline="central" text-anchor="middle">{self.split_feature}</text>'
+        ret += "</svg>"
+        return ret
+
+    def subtree_svg(self, root_labels):
+        '''Returns an SVG string of the decision tree node (and its children recursively) along with it's width & height. Used to compute the SVG for the whole decision tree.'''
+        # Base case: We return a single node withou
+        if self.children == None:
+            return self.node_svg(0,0,root_labels), 80, 120
+        
+        children = [child.subtree_svg(root_labels) for child in self.children.values()]
+        children_widths = [child[1] for child in children]
+        children_heights = [child[2] for child in children]
+        width = sum(children_widths)
+        height = max(children_heights)+140
+        ret = f'<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 {width} {height}" width="{width}" height="{height}">'
+        x_pos = 0
+        for child, cwidth, _ in children:
+            ret += f'<svg y="140" x="{x_pos}"' + child[4:]
+            ret += f'<line stroke="#444444" stroke-width="2" x1="{width/2}" y1="{100}" x2="{x_pos + (cwidth/2)}" y2="140"/>\n'
+            x_pos += cwidth
+
+        ret += self.node_svg(width/2-40,0, root_labels)
+        ret += "</svg>"
+        return ret, width, height
+    
+    def _repr_svg_(self):
+        return self.node_svg(0,0,self.labels)
 
 class DecisionTree:
     
@@ -261,13 +393,18 @@ class DecisionTree:
             feature_names = self.feature_names
         else:
             feature_names = list(range(len(self.X[0])))
-        print(self.X)
-        print(self.y)
+
         self.root = _DecisionTreeNode(self.X,self.y, feature_names, [True]*len(feature_names))
         self.root.id3(min_gain=self.min_gain, min_instances=self.min_instances, max_depth=self.max_depth)
 
-    def predict(self, X):
+    def predict(self, X = None, data = None):
         '''Use the decision tree to predict a label for each of the given instances.'''
+        if X is None and data is None:
+            raise Exception("No instance was given. Please pass a list of instances to `X` or a DataFrame of instances to `data`.")
+        if X is not None and data is not None:
+            raise Exception("Both a dataframe and a list of instances was used. Please only pass a value to one of them.")
+        if data is not None:
+            X = data.loc[:,self.feature_names].values.tolist()
         ret = []
         for x in X:
             ret.append(self._predict_instance(x))
@@ -286,3 +423,53 @@ class DecisionTree:
                 break
             currentNode = currentNode.children[x[split_index]]
         return currentNode.getNodeMajorityLabel()
+    
+    def _legend_svg(self):
+        '''Returns an SVG string of the legend, along with it's width and height'''
+        labels = sorted(self.root.labels)
+        y_offset = 0
+        height = 20*len(labels) 
+        if self.target is not None:
+            y_offset = 25
+            height += 30
+        width = 150 
+        svgstr = f"""<svg width="{width+2}" height="{height+2}" xmlns="http://www.w3.org/2000/svg">
+        <rect rx="5" ry="5" width="{width}" height="{height}" x="1" y="1" stroke="#AAAAAA" stroke-width="2" fill="white"/>"""
+        if self.target is not None:
+            svgstr +=f'<text x="75" y="{15}" font-family="Arial, Helvetica, sans-serif" font-weight="bold" dominant-baseline="central" text-anchor="middle">{self.target}</text>'
+        for i, (color, label) in enumerate(zip(_color_palette(len(labels)), labels)):
+            svgstr += f'<line stroke="{color}" stroke-width="2.5" x1="10" x2="30" y1="{10+i*20+y_offset}" y2="{10+i*20+y_offset}"/>'
+            svgstr +=f'<text x="35" y="{10+i*20 + y_offset}" font-family="Arial, Helvetica, sans-serif" dominant-baseline="central">{label}</text>'
+
+        svgstr += "</svg>"
+        return svgstr, width+2, height+2
+    
+    def _repr_svg_(self):
+        '''Returns an SVG string of the tree'''
+        tree = self.root.subtree_svg(root_labels=self.root.labels)
+        legend = self._legend_svg()
+        # Since we will translate the legend by 10 in y, we translate it all the way to the side if it gets higher that 110
+        if legend[2] >= 110:
+            width = tree[1] + legend[1]
+            legend_offset = tree[1]
+        else:
+            width = max(tree[1], tree[1]/2 + 50 + legend[1])
+            legend_offset = tree[1]/2 + 50
+        
+        height = max(tree[2], legend[2])
+        ret = f'''<svg width="{width+10}" height="{height}" xmlns="http://www.w3.org/2000/svg">'''
+        ret += f'<rect width="{100}%" height="{100}%" fill="white"/>'
+        # Adding the tree
+        ret += tree[0]
+        # Adding and translating the legend
+        ret += f'''<svg width="{legend[1]}" height="{legend[2]}" x="{legend_offset}" y="10" xmlns="http://www.w3.org/2000/svg">'''
+        ret += legend[0]
+        ret += "</svg>"
+        ret += "</svg>"
+        return ret
+    
+    def savefig(self, fname):
+        '''Saves a file of the tree's SVG representation to the specified file path.
+        - `fname` the file path of the file to be created.'''
+        with open(fname, "w") as f:
+            f.write(self._repr_svg_())
